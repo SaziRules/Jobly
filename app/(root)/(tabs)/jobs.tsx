2d5cb9c903e4ff08559b9f82b877395a456fe1bc
Sazi <sazi@Sazis-MacBook-Air.local>
2024-09-30 10:52:52 +0200

    Initial Commit

---
+++ app/(root)/(tabs)/jobs.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Jobs = () => {
+    return (
+        <SafeAreaView>
+        <Text>Jobs</Text>
+    </SafeAreaView>
+    );  
+};
+
+export default Jobs;
